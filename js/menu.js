e2137585d5cb1d10e3adfb9ecfcc8f1eac627a65
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2023-01-09 23:26:26 +0900

    メニュー現在開いているページ表示機能

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+var viewer = document.getElementById("viewer");
+
 $(document).ready(function () {
     // 「戻る」ボタンを押したとき
     $("button.back").on("click", function () {
@@ -11,4 +13,17 @@ $(document).ready(function () {
         const content = ck.split('=');
         Cookies.remove(content[0]);
     }
+
+    // 現在iframeで開いているページのメニュー項目を色変え
+    $(iframe_main).on("load", function () {
+        $("#menu a").each(function (i, elem) {
+            if ($(elem).attr("href") === viewer.contentWindow.location.href) {
+                // 現在iframe_mainで開いているページに対応するメニューの項目について
+                $(elem).addClass("now_open");
+            } else {
+                $(elem).removeClass("now_open");
+            }
+        });
+    });
 });
+

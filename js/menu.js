494c5dc10437f4101bcc245215191b2811f2e743
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2023-01-09 20:37:48 +0900

    テスト

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,8 @@ $(window).on("load", function () {
     console.log("delete!");
     const cookies = document.cookie.split(';')
     for (let i = 0; i < cookies.length; i++) {
-        console.log("delete2!");
         const cookie = cookies[i]
+        console.log(cookie);
         const eqPos = cookie.indexOf('=')
         const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
         document.cookie = name + '=;max-age=0'

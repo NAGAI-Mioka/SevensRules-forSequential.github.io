58c13446721b299962c649b1e6e1f60fb576986a
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2023-01-09 20:48:41 +0900

    テスト

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,9 @@ $(window).on("load", function () {
     for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i].trim()
         console.log(cookie);
-        //const eqPos = cookie.indexOf('=')
-        //const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
-        document.cookie = cookie + ';max-age=0'
+        const eqPos = cookie.indexOf('=')
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+        console.log(name);
+        document.cookie = cookie + '; max-age=0'
     }
 });

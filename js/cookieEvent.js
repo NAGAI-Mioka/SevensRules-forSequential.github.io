2d04fb30f2c87e2ebb16d104404c0c10e65173b2
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2023-01-09 19:24:40 +0900

    既読機能を追加

---
+++ js/cookieEvent.js
@@ -0,0 +1,18 @@
+// 一度見たページのデザインを変える
+
+// 描画されたとき
+$(window).on("load", function () {
+    if (Cookies.get("visited-" + window.location.pathname) != null) {
+        $("body, h1").removeClass("unvisited").addClass("visited");
+    } else {
+        $("body, h1").removeClass("visited").addClass("unvisited");
+    }
+});
+
+// iframe_mainの表示から外れたとき
+$(window).on("unload", function () {
+    if (Cookies.get("visited-" + window.location.pathname) == null) {
+        Cookies.set("visited-" + window.location.pathname, "true");
+    }
+});
+
